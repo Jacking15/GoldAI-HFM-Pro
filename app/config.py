5e4c9fb39b702bea4b7d5d3178812fab37c426e6
Jacking15 <112776376+Jacking15@users.noreply.github.com>
2026-06-29 17:38:30 +0700

    Create app/config.py

---
+++ app/config.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    TELEGRAM_BOT_TOKEN = os.getenv("8727899215:AAE_QzyzgEg-O0bcN-6DVb5YvKuTqSQmcD4")
+    TELEGRAM_CHAT_ID = os.getenv("@TrenSignal_Bot")
+
+    MODE = os.getenv("MODE", "SIGNAL")
+    RISK_PERCENT = float(os.getenv("RISK_PERCENT", 1))
+
+    SYMBOL = os.getenv("SYMBOL", "XAUUSD")
+    TIMEFRAMES = os.getenv("TIMEFRAMES", "M1,M5,M15").split(",")
