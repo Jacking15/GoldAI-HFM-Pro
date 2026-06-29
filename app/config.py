2919a472577c47b1e414ceca1d2651670841dbcb
Jacking15 <112776376+Jacking15@users.noreply.github.com>
2026-06-29 19:57:44 +0700

    Update config.py

--- app/config.py
+++ app/config.py
@@ -4,8 +4,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-    TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+    TELEGRAM_BOT_TOKEN = os.getenv("8727899215:AAE_QzyzgEg-O0bcN-6DVb5YvKuTqSQmcD4")
+    TELEGRAM_CHAT_ID = os.getenv("TrenSignal_Bot")
 
     MODE = os.getenv("MODE", "SIGNAL")
     RISK_PERCENT = float(os.getenv("RISK_PERCENT", 1))
